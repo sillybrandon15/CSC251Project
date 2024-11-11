5983ff37bd204105e1c617df90244366a0cd69c2
sillybrandon15 <bcarbaj2@email.cpcc.edu>
2024-11-11 01:15:34 -0500

    Update Policy.java
    Added documentation comments

--- Policy.java
+++ Policy.java
@@ -26,8 +26,18 @@ public class Policy
       holderHeight = 0;
       holderWeight = 0;
    }
-   
-   //Constructor that accepts all relevant arguments to fully initialize the instance
+   /**
+    * Constructor that accepts all relevant arguments to fully initialize the instance
+    *
+    * @param num The user's policy number
+    * @param provider The user's policy provider company
+    * @param firstName The policyholder's first name
+    * @param lastName The policyholder's last name
+    * @param age The policyholder's age
+    * @param smoking The policyholder's smoking status (either "smoker" or "non-smoker")
+    * @param height The policyholder's height
+    * @param weight The policyholder's weight
+    */
    public Policy(int num, String provider, String firstName, 
                  String lastName, int age, String smoking, 
                  double height, double weight)
@@ -46,49 +56,81 @@ public class Policy
    ------------Methods(Mutator)--------------
    */
    
-   //Sets the policy number
+   /**
+    * Sets the policy number
+    * 
+    * @param num The user's policy number 
+    */
    public void setPolicyNumber(int num)
    {
       policyNumber = num;
    }
    
-   //Sets the policy provider's name
+   /**
+    * Sets the policy provider's name
+    *
+    * @param provider The user's policy provider company
+    */
    public void setProviderName(String provider)
    {
       providerName = provider;
    }
    
-   //Sets the policyholder's first name
+   /**
+    * Sets the policyholder's first name
+    *
+    * @param firstName The policyholder's first name
+    */
    public void setHolderFirstName(String firstName)
    {
       holderFirstName = firstName;
    }
    
-   //Sets the policyholder's last name
+   /**
+    * Sets the policyholder's last name
+    *
+    * @param lastName The policyholder's last name
+    */
    public void setHolderLastName(String lastName)
    {
       holderLastName = lastName;
    }
    
-   //Sets the policyholder's age
+   /**
+    * Sets the policyholder's age
+    *
+    * @param age The policyholder's age
+    */
    public void setHolderAge(int age)
    {
       holderAge = age;
    }
    
-   //Sets the policyholder's smoking status | "smoker" or "non-smoker"
+   /**
+    * Sets the policyholder's smoking status | "smoker" or "non-smoker"
+    *
+    * @param smoking The policyholder's smoking status (either "smoker" or "non-smoker")
+    */
    public void setHolderSmokingStatus(String smoking)
    {
       holderSmokingStatus = smoking;
    }
    
-   //Sets the policyholder's height
+   /**
+    * Sets the policyholder's height
+    *
+    * @param height The policyholder's height
+    */
    public void setHolderHeight(double height)
    {
       holderHeight = height;
    }
    
-   //Sets the policyholder's weight
+   /**
+    * Sets the policyholder's weight
+    *
+    * @param weight The policyholder's weight
+    */
    public void setHolderWeight(double weight)
    {
       holderWeight = weight;
@@ -98,49 +140,81 @@ public class Policy
    ----------Methods(Accessors)---------------
    */
    
-   //Gets the policy number
+   /**
+    * Gets the policy number
+    * 
+    * @return An int of the current policy number
+    */
    public int getPolicyNumber()
    {
       return policyNumber;
    }
    
-   //Gets the policy provider's name
+   /**
+    * Gets the policy provider's name
+    * 
+    * @return A string of the current provider's name
+    */
    public String getProviderName()
    {
       return providerName;
    }
    
-   //Gets the policyholder's first name
+   /**
+    * Gets the policyholder's first name
+    *
+    * @return A string of the current holder's first name
+    */
    public String getHolderFirstName()
    {
       return holderFirstName;
    }
    
-   //Gets the policyholder's last name
+   /**
+    * Gets the policyholder's last name
+    *
+    * @return A String of the current holder's last name
+    */
    public String getHolderLastName()
    {
       return holderLastName;
    }
    
-   //Gets the policyholder's age
+   /**
+    * Gets the policyholder's age
+    *
+    * @return An int of the current holder's age
+    */
    public int getHolderAge()
    {
       return holderAge;
    }
    
-   //Gets the policyholder's smoking status | "smoker" or "non-smoker"
+   /**
+    * Gets the policyholder's smoking status | "smoker" or "non-smoker"
+    *
+    * @return A String containing either "smoker" or "non-smoker"
+    */
    public String getHolderSmokingStatus()
    {
       return holderSmokingStatus;
    }
    
-   //Gets the policyholder's height
+   /**
+    * Gets the policyholder's height
+    *
+    * @return A double containing the current holder's height
+    */
    public double getHolderHeight()
    {
       return holderHeight;
    }
    
-   //Gets the policyholder's weight
+   /**
+    * Gets the policyholder's weight
+    *
+    * @return A double containing the current holder's weight
+    */
    public double getHolderWeight()
    {
       return holderWeight;
@@ -150,13 +224,21 @@ public class Policy
    ----------Methods(Other)---------------
    */
    
-   //Calculates and returns the policyholder's BMI
+   /**
+    * Calculates and returns the policyholder's BMI
+    *
+    * @return The approximate BMI value using the current holder's height and weight
+    */
    public double calculateBMI()
    {
       return ((holderWeight * 703) / (holderHeight * holderHeight));
    }
    
-   //Calculates and returns the price of the policy
+   /**
+    * Calculates and returns the price of the policy
+    *
+    * @return A double containing the price of the policy based on the current holder's weight and height
+    */
    public double calculatePolicyPrice()
    {
       //Declaring the variables
